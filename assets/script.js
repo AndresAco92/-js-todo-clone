60cffc56abf44390710abf80a8acfbad5c2a0ae2
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-04-22 18:27:23 +0200

    Esercizio finito...

--- assets/script.js
+++ assets/script.js
@@ -17,4 +17,17 @@ $(function(){
   $(document).on('click', '.fa-trash', function(){
     $(this).parent().remove();
   });
+
+  $('.list input').keyup(function(event){
+    if(event.which === 13){
+      var lista_read = $(this).val().trim(); 
+      if(lista_read.length > 3){
+        var clone_templete = $('.templete_prova li').clone();
+        $(clone_templete).find('.list_text').append(lista_read);
+        $('.scale ul').append(clone_templete);
+        $(this).val('');
+      }
+    }
+
+  });
 });
